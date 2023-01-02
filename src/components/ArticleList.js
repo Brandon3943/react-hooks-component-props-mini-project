155e4fe1c1299ec8b8227ff18cfc80a068d3c78e
root <root@Ty-Laptop.localdomain>
2023-01-01 23:47:57 -0500

    completed assignment

---
+++ src/components/ArticleList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Article from "./Article"
+
+
+function ArticleList({ posts }) {
+    
+
+    const post = posts.map(article => {
+        return <Article key={article.id} title={article.title} date={article.date} preview={article.preview} />
+    })
+
+    console.log(post)
+  return (
+    <div>
+        <main>
+            {post}
+        </main>
+    </div>
+  )
+}
+
+export default ArticleList
